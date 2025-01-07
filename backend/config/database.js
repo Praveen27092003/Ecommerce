42007994340a41e64ef22387eb28bd1ea6b07163
Praveen27092003 <praveenbaskar27@gmail.com>
2025-01-07 13:10:00 +0530

    initial message

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,12 +1,19 @@
 const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+const connectDatabase = () => {
 
-const connectDatabase = ()=>{
-    mongoose.connect(process.env.DB_LOCAL_URI,{
-        useNewUrlParser:true,
-        useUnifiedTopology:true
-    }).then(con=>{
-        console.log(`MongoDB is connected to the host: ${con.connection.host} `)
+  mongoose
+    .connect(process.env.DB_LOCAL_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     })
-}
+    .then((con) => {
+      console.log(`MongoDB is connected to the host: ${con.connection.host}`);
+    })
+    .catch((err) => {
+      console.error('MongoDB Connection Error:', err);
+      process.exit(1);  // Exit the process if MongoDB connection fails
+    });
+};
 
 module.exports = connectDatabase;
